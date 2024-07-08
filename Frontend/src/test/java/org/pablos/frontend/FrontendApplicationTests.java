8e460a4d855798bd9770eb73dd66e29694442f74
Pablos <pablos@list.ru>
2024-07-08 14:32:04 +0300

    init commit. structure frame ready

---
+++ Frontend/src/test/java/org/pablos/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pablos.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
